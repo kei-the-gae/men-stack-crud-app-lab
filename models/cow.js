0ba34ca55f783531c27b16e98131cc3ab24642ad
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-28 18:04:34 -0700

    Add cow model

---
+++ models/cow.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const cowSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    gender: { type: String, required: true },
+    description: { type: String, required: true },
+    image: String,
+});
+
+const Cow = mongoose.model('Cow', cowSchema);
+module.exports = Cow;
